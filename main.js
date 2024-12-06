9a8b77482c96d36a2b4d2d8199e7ce27417f2698
adomboua <allan-yoropo-kelyn.domboua@etu.u-bordeaux.fr>
2024-12-06 05:48:27 +0100

    version stable

--- main.js
+++ main.js
@@ -6,11 +6,15 @@ function switch_presentation(subject) {
   const coral_reef = document.getElementById("coral-reef");
   const whale_migration = document.getElementById("whale-migration");
   const ocean_pollution = document.getElementById("ocean-pollution");
-  console.log("Switching");
   coral_reef.style.display = "none";
   whale_migration.style.display = "none";
   ocean_pollution.style.display = "none";
+  coral_reef.classList.remove('show');
+  ocean_pollution.classList.remove('show');
+  whale_migration.classList.remove('show');
+
   document.getElementById(subject).style.display = "block";
+  animation(subject);
 }
 function switch_nervous_system() {
   switch_presentation("whale-migration");
@@ -21,3 +25,12 @@ function switch_skeleton() {
 function switch_toxins() {
   switch_presentation("ocean-pollution");
 }
+function animation(subject) {
+  const element1 = document.getElementById(subject)
+  setTimeout(() => {
+    element1.classList.add('show');
+  }, 200);
+}
+
+
+
